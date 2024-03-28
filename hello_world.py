797210f46fe30e88f7542db483aae148dcffe6c6
Noob <noob@mail.com>
2024-03-28 14:21:38 +0500

    Удаление и превращение

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print('Привет мир!')
+
+
+password = '5i2309tj90tu4309tu3409t vnu5409tyun5830ty954tu340'
+
+name = "glremghiorthgioeng[oi]"
+
+email = "golkerj@mfgmsg@voidjgn.com"
+
+game = game_over()
+
